b6d4633eaed40c3686e51d707616a0b49ba98e97
Skilltank01 <developer@skilltank.tech>
2024-01-08 20:09:33 +0530

    add deploy link

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || "https://url-shortner-zptf.vercel.app/";
 
 app.use(bodyParser.json());
 app.use(
